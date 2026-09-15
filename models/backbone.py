55ca22eaacc3aae62a6745736f8a8a29c22b8533
gildas.evano@student-cs.fr <gildas.evano@student-cs.fr>
2026-09-15 18:49:12 +0200

    building and freezing backbone

--- models/backbone.py
+++ models/backbone.py
@@ -1,8 +1,19 @@
 import torch
-import numpy as np
 
-def build_backbone():
-    pass
+def build_backbone(encoder_name):
+    """Loads and returns the encoder (dino vitb 16)
 
-if __name__ == "__main__":
-    from config import CONFIG
+    All parameters have requires_grad set to False"""
+    backbone = torch.hub.load(*encoder_name, trust_repo=True)
+    for _, param in backbone.named_parameters():
+        param.requires_grad = False
+    
+    return backbone
+
+def unfreeze_vit_from(backbone, from_num):
+    """Sets the requires_grad of the parameters of the blocks starting from the number from_num to True (inplace)"""
+    for name, param in backbone.named_parameters():
+        if name[:6] == 'blocks':
+            num = int(name.split('.')[1])
+            if num >= from_num:
+                param.requires_grad = True
